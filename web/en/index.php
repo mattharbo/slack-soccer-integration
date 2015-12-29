29638eabb21550f84525be1d1a78ac727fccc247
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2015-12-29 18:24:14 +0100

    Refactor
    - Adding current (server) time display
    - Removing no result slack message

--- web/en/index.php
+++ web/en/index.php
@@ -23,6 +23,8 @@ $yesterday = date('Y-m-d',strtotime($currentdayprep . "-1 days"));
 echo "Yesterday was : ".$yesterday."<br>";
 
 $currenttime=date('H:i');
+echo "Current time is : ".$currenttime."<br><br>";
+
 $index=0;
 $gamelist = array('gamefakeid' => array(),'journey' => array(),'hometeam' => array(), 'scorehome' =>  array(), 'awayteam' => array(), 'scoreaway' => array());
 
@@ -75,7 +77,7 @@ echo $concatgamelist;
 if ($concatgamelist != NULL) {
 	###################### Sending data to Slack channel #####################
 
-	//API Url
+	//Hook Url
 	//$url = 'https://hooks.slack.com/services/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'; //ligue1
 	//$url='https://hooks.slack.com/services/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'; //championsleague
 	$url = 'https://hooks.slack.com/services/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'; //premierleague
@@ -110,8 +112,7 @@ if ($concatgamelist != NULL) {
 	//Execute the request
 	$result = curl_exec($ch);
 
-	// Step by step explanation of the above code:
-
+	// Step by step explanation of the code above:
 	// We setup the URL that we want to send our JSON to.
 	// We initiated cURL using curl_init.
 	// We setup a PHP array containing sample data.
@@ -121,31 +122,6 @@ if ($concatgamelist != NULL) {
 	// We set the content-type of our request to application/json. It is extremely important to note that you should always use “application/json”, not “text/json”. Simply put, using “text/json” is incorrect!
 	// Finally, we used the function curl_exec to execute our POST request. If you want to check for errors at this stage, then you should check out my article on error handling with cURL.
 }else{
-	$url = 'https://hooks.slack.com/services/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'; //premierleague
-	 
-	//Initiate cURL.
-	$ch = curl_init($url);
-
-	$jsonData = [
-	    'attachments' => [[
-	    	'fallback' => 'No result available for',
-	    	'pretext' => "@channel : No result available for :flag-fr:",
-		]]//end attachments
-	];
-	 
-	//Encode the array into JSON.
-	$jsonDataEncoded = json_encode($jsonData);
-	 
-	//Tell cURL that we want to send a POST request.
-	curl_setopt($ch, CURLOPT_POST, 1);
-	 
-	//Attach our encoded JSON string to the POST fields.
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-	 
-	//Set the content type to application/json
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-	 
-	//Execute the request
-	$result = curl_exec($ch);
+	echo "No results available now.";
 }
 ?>
