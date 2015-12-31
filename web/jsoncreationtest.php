68970b541b598d972ea6b518d0c3b03773d7baa2
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2015-12-31 15:59:18 +0100

    Json backup preparation

---
+++ web/jsoncreationtest.php
@@ -0,0 +1,6 @@
+<?
+include './includes.php';
+
+//todaysjsonbackup("jsonfilename","date","status","matchday","homeTeamName","awayTeamName","goalsHomeTeam","goalsAwayTeam");
+todaysjsonbackup("gamesbackup","1234","termine","17","Paris","Lille","2","0");
+?>
